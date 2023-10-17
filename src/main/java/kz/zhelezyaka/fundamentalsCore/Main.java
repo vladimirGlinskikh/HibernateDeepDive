f3702cc5d6d05889fe8bdc4cfa0488b3aea25a15
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-10-17 18:33:22 +0300

    Entity Persistence by using class not a file xml

--- src/main/java/kz/zhelezyaka/fundamentalsCore/Main.java
+++ src/main/java/kz/zhelezyaka/fundamentalsCore/Main.java
@@ -4,18 +4,27 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import kz.zhelezyaka.fundamentalsCore.entities.Product;
+import kz.zhelezyaka.fundamentalsCore.persistence.CustomPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+//        EntityManagerFactory entityManagerFactory =
+//                Persistence.createEntityManagerFactory("persistence");
+
         EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("persistence");
+                new HibernatePersistenceProvider()
+                        .createContainerEntityManagerFactory(
+                                new CustomPersistenceUnitInfo(), new HashMap<>());
 
         try (EntityManager entityManager =
                      entityManagerFactory.createEntityManager()) {
             entityManager.getTransaction().begin();
 
             Product product = new Product();
-            product.setName("Cognac");
+            product.setName("Champagne");
             entityManager.persist(product);
             entityManager.getTransaction().commit();
         }
